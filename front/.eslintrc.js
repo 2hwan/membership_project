6e559289aa5978cb04812b9cefabe7f41b3fecec
sj <dbstjdwo0000@gmail.com>
2020-07-20 16:46:21 +0900

    eslint 추가

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,28 +1,28 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true
-    },
-    extends: ["plugin:vue/essential", "@vue/prettier"],
-    parserOptions: {
-        parser: "babel-eslint"
-    },
-    rules: {
-        "no-console": "off",
-        // "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-        // "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-        "prettier/prettier": [
-            "error",
-            {
-                singleQuote: true,
-                semi: true,
-                useTabs: true,
-                tabWidth: 4,
-                trailingComma: "none",
-                printWidth: 200,
-                bracketSpacing: true,
-                arrowParens: "avoid"
-            }
-        ]
-    }
+	root: true,
+	env: {
+		node: true
+	},
+	extends: ['plugin:vue/essential', '@vue/prettier'],
+	parserOptions: {
+		parser: 'babel-eslint'
+	},
+	rules: {
+		'no-console': 'off',
+		// "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		// "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				semi: true,
+				useTabs: true,
+				tabWidth: 4,
+				trailingComma: 'none',
+				printWidth: 200,
+				bracketSpacing: true,
+				arrowParens: 'avoid'
+			}
+		]
+	}
 };
