9b91a70e7eec635622af949a0e38f8fb71bbff5e
sj <dbstjdwo0000@gmail.com>
2020-07-20 16:46:39 +0900

    로그인 api, interceptor 추가

---
+++ front/src/api/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { setInterceptors } from '@/api/interceptors';
+
+function createInstance() {
+	const instance = axios.create({
+		baseURL: 'http://localhost:8080'
+	});
+	return setInterceptors(instance);
+}
+
+const instance = createInstance();
+
+export function signupUser(userData) {
+	return instance.post('signup', userData);
+}
+
+export function loginUser(userData) {
+	return instance.post('login', userData);
+}
---
+++ front/src/api/interceptors.js
@@ -0,0 +1,14 @@
+import store from '@/store/index';
+
+export function setInterceptors(instance) {
+	instance.interceptors.request.use(
+		function(config) {
+			config.headers.Authorization = store.state.token;
+			return config;
+		},
+		function(error) {
+			return error;
+		}
+	);
+	return instance;
+}
