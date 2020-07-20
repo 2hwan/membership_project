6b8c1bb562568c6e20e26b8f641934e6d92c3e36
sj <dbstjdwo0000@gmail.com>
2020-07-20 16:47:40 +0900

    메인페이지 구현

--- front/src/views/MainPage.vue
+++ front/src/views/MainPage.vue
@@ -1,11 +1,25 @@
 <template>
-	<div>
-		main
+	<div class="container mt-5" style="height: 50%">
+		<mdb-carousel :interval="8000" slide :items="items" controlls style="height: 50%"></mdb-carousel>
 	</div>
 </template>
 
 <script>
+import { mdbCarousel } from 'mdbvue';
 export default {
-	name: 'MainPage'
+	name: 'MainPage',
+	components: {
+		mdbCarousel
+	},
+	data() {
+		return {
+			items: [
+				{
+					img: true,
+					src: 'https://zone.membership.kt.com/event/DoubleDiscount/img/2007/visual.jpg'
+				}
+			]
+		};
+	}
 };
 </script>
