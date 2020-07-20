3b4b76ecbf51af3dfef9d8adc1a34cc88a3f60ae
sj <dbstjdwo0000@gmail.com>
2020-07-20 16:47:01 +0900

    main import 변경

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,16 @@
 <template>
 	<div id="app">
+		<AppBar></AppBar>
 		<router-view />
 	</div>
 </template>
 
-<style></style>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
+</style>
+<script>
+import AppBar from '@/components/common/AppBar';
+export default {
+	components: { AppBar }
+};
+</script>
