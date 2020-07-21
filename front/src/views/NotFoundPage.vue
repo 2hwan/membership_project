7ed915658ce6fb92ee01873fd15896ef09368029
sj <dbstjdwo0000@gmail.com>
2020-07-21 16:48:39 +0900

    not found page

--- front/src/views/NotFoundPage.vue
+++ front/src/views/NotFoundPage.vue
@@ -1,40 +1,13 @@
 <template>
 	<div class="text-center">
-		<h1>{{ name }}</h1>
-		<button class="button-my" v-on:click="changeName">click</button>
-		<button v-on:click="addNumber">click</button><input type="text" v-model="data" @blur="alertData" />
-		<div v-for="(l, index) in list" :key="index">
-			{{ l }}
-		</div>
+		Page Not Found
 	</div>
 </template>
 
 <script>
 export default {
-	name: 'NotFoundPage',
-	data() {
-		return {
-			name: '',
-			data: '',
-			list: [1, 2, 3, 3242315, 3]
-		};
-	},
-	methods: {
-		addNumber() {
-			this.list.push(5);
-		},
-		changeName() {
-			this.name = '윤성재';
-		},
-		alertData() {
-			console.log(this.data);
-		}
-	}
+	name: 'NotFoundPage'
 };
 </script>
 
-<style scoped>
-.button-my {
-	color: red;
-}
-</style>
+<style scoped></style>
