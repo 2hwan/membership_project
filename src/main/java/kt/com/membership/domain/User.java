bb4987c9a8ef312c715daf45f8b556a7673abf70
sj <dbstjdwo0000@gmail.com>
2020-07-27 14:58:06 +0900

    security 수정

--- src/main/java/kt/com/membership/domain/User.java
+++ src/main/java/kt/com/membership/domain/User.java
@@ -5,15 +5,18 @@ import kt.com.membership.dto.LoginForm;
 import kt.com.membership.dto.UserForm;
 import kt.com.membership.exception.InvalidPasswordException;
 import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Getter
-public class User {
+public class User implements UserDetails {
 
     @Id
     @Column(nullable = false, unique = true, length = 20)
@@ -43,6 +46,36 @@ public class User {
 
     @ElementCollection(fetch = FetchType.EAGER)
     private List<String> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 
     public User() {
     }
@@ -89,4 +122,5 @@ public class User {
     public void signUpMembership(){
         this.isMembershipUser = true;
     }
+
 }
